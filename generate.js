c617f66f742684492ad3700cc4638d37eee8e1cc
fun <lljxx1@gmail.com>
2023-01-07 21:55:46 +0800

    add

--- generate.js
+++ generate.js
@@ -6,6 +6,7 @@ const allFile = __dirname + "/blacklist/all.json";
 const domainFile = __dirname + "/blacklist/domains.json";
 const addressFile = __dirname + "/blacklist/address.json";
 const combinedFile = __dirname + "/blacklist/combined.json";
+const archiveDir = __dirname + "/blacklist/archive/";
 
 async function getRecentScamActivity(limit = 100) {
   const req = await fetch(
@@ -15,6 +16,7 @@ async function getRecentScamActivity(limit = 100) {
   let allAddressList = [];
   let allSites = [];
   let combined = {};
+  let indexByDay = {};
   list.map((_) => {
     const actions = _.action.split(',');
     if (actions.indexOf('maliciousCodeFeature') > -1 && actions.length === 1) {
@@ -30,16 +32,33 @@ async function getRecentScamActivity(limit = 100) {
     );
     if (_.host) allSites.push(_.host);
 
+    const day = moment(_.time).format("YYYY-MM-DD");
+
+    indexByDay[day] = indexByDay[day] || {
+      domain: [],
+      address:[]
+    };
+
+    indexByDay[day].domain.push(_.host);
     combined[_.host] = combined[_.host] || [];
 
     list.forEach(address => {
+      indexByDay[day].address.push(address);
       if (combined[_.host].indexOf(address) === -1) {
         combined[_.host].push(address);
       }
     })
   });
   return {
-    newCombined: combined,
+    days: Object.keys(indexByDay).map(day => {
+      const data = indexByDay[day];
+      return {
+        day,
+        domains: Array.from(new Set(data.domain)),
+        address: Array.from(new Set(data.address)),
+      }
+    }),
+    // newCombined: combined,
     address: Array.from(new Set(allAddressList)),
     domains: Array.from(new Set(allSites)),
   };
@@ -51,18 +70,21 @@ async function doGenerate(lastId = 1) {
     domains: [],
     address: []
   };
-  if (fs.existsSync(allFile)) {
+  if (fs.existsSync(allFile) && fs.existsSync(archiveDir)) {
     console.log('load')
     cacheData = JSON.parse(fs.readFileSync(allFile, "utf-8"));
   } else {
     firstRun = true;
   }
 
+  if (!fs.existsSync(archiveDir)) {
+    fs.mkdirSync(archiveDir);
+  }
+
   cacheData.combined = cacheData.combined || {};
 
-  const limit = firstRun ? 50000 : 2000;
+  const limit = firstRun ? 100000 : 2000;
   const allList = await getRecentScamActivity(limit);
-
 
   allList.combined = firstRun ? {} : cacheData.combined;
 
@@ -70,6 +92,8 @@ async function doGenerate(lastId = 1) {
   const newAddress = [];
 
   // console.log("firstRun", firstRun, allFile);
+  console.log(allList.days[0])
+  return 
 
   allList.domains.forEach((domain) => {
     if (cacheData.domains.indexOf(domain) === -1) {
@@ -77,19 +101,19 @@ async function doGenerate(lastId = 1) {
     }
   });
 
-  for(const host in allList.newCombined) {
-    const existList = cacheData.combined[host] || [];
-    const combinedAddress = allList.newCombined[host] || [];
-    combinedAddress.forEach((address) => {
-      if (existList.indexOf(address) === -1) {
-        existList.push(address);
-      }
-    });
+  // for(const host in allList.newCombined) {
+  //   const existList = cacheData.combined[host] || [];
+  //   const combinedAddress = allList.newCombined[host] || [];
+  //   combinedAddress.forEach((address) => {
+  //     if (existList.indexOf(address) === -1) {
+  //       existList.push(address);
+  //     }
+  //   });
 
-    if (existList.length) {
-      allList.combined[host] = existList;
-    }
-  }
+  //   if (existList.length) {
+  //     // allList.combined[host] = existList;
+  //   }
+  // }
 
   allList.address.forEach((address) => {
     if (cacheData.address.indexOf(address) === -1) {
